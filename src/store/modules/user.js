32092b5f6965e966d2f19d6d97e32416a7ce4dfd
杨小伟 <2389469307@qq.com>
2021-04-27 00:02:18 +0800

    update: 退出

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -79,20 +79,21 @@ const actions = {
   // user logout
   logout({ commit, state, dispatch }) {
     return new Promise((resolve, reject) => {
-      logout(state.token).then(() => {
-        commit('SET_TOKEN', '')
-        commit('SET_ROLES', [])
-        removeToken()
-        resetRouter()
+      commit('SET_TOKEN', '')
+      commit('SET_ROLES', [])
+      removeToken()
+      resetRouter()
+      localStorage.clear()
 
-        // reset visited views and cached views
-        // to fixed https://github.com/PanJiaChen/vue-element-admin/issues/2485
-        dispatch('tagsView/delAllViews', null, { root: true })
+      // reset visited views and cached views
+      dispatch('tagsView/delAllViews', null, { root: true })
 
-        resolve()
-      }).catch(error => {
-        reject(error)
-      })
+      resolve()
+      // logout(state.token).then(() => {
+      //
+      // }).catch(error => {
+      //   reject(error)
+      // })
     })
   },
 
